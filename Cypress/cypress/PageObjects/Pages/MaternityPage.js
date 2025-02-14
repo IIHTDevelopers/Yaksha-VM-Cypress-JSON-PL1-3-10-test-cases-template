d255a3b0b322ad9cf8bb322b306390653c42af5b
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-02-14 14:22:51 +0530

    Update MaternityPage.js

--- Cypress/cypress/PageObjects/Pages/MaternityPage.js
+++ Cypress/cypress/PageObjects/Pages/MaternityPage.js
@@ -7,8 +7,6 @@ export default class MaternityPage {
       maternityAllowanceReport: '',
       dateFrom: '',
       showReportBtn: '',
-      tableRowLocator: '',
-      createdOn: '',
       dataType: "",
     };
   }
